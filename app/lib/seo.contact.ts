a62eb3174f030ff2cfb54778fd2437d59188d8a3
Hineoc VC <hineoc@HineocdeMacBook-Air.local>
2025-11-12 17:37:35 +0800

    Changed WA number

--- app/lib/seo.contact.ts
+++ app/lib/seo.contact.ts
@@ -4,5 +4,5 @@ export const CONTACT_SEO_BASE = {
   twitter: '@firedragon',
   logo: '/logo-3-topic.png',
   emailTo: 'firedragonco2010@gmail.com',   // 表单 mailto 目标地址
-  waNumber: '60189501638',           // 不带 + 的国际格式，例：60 开头
+  waNumber: '601139387226',           // 不带 + 的国际格式，例：60 开头
 }
